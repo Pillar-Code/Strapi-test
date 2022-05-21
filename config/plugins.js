2e377f65e98986e2719ed1ce1a8749ed88feefcd
Arthur Lemos <arthurpolon@hotmail.com>
2022-05-21 00:49:50 -0300

    📦 NEW: add TitapEditor and Transformer plugins

--- config/plugins.js
+++ config/plugins.js
@@ -22,4 +22,14 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    'transformer': {
+      enabled: true,
+      config: {
+        prefix: '/api/',
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        }
+      }
+    },
   });
